04da1e6de75d1b0684fbf5b2dea62741a9f0dcf5
Anna Morozova <anna.mik.morozova@gmail.com>
2020-07-10 00:24:38 -0400

    Feat: add randomNumber func

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function reverseString(str) {
    return str.split("").reverse().join("")
 }
 
+function randomNumber(min, max) {  
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
 module.exports = {
     reverseString,
-    capitalize
+    capitalize,
+    randomNumber
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,24 +1,31 @@
 const expect = require("chai").expect;
-const { capitalize, reverseString } = require("../index");
+const { capitalize, reverseString, randomNumber } = require("../index");
 
-describe('capitalize function', function() {
-  it('should capitalize first letter of a string', () => {
+describe("capitalize function", function() {
+  it("should capitalize first letter of a string", () => {
     const result = capitalize("flavio");
     expect(result).to.have.lengthOf(6)
     expect(result).to.equal("Flavio");
 });
 
-  it('should return empty string if the value passed is not a string', () => {
+  it("should return empty string if the value passed is not a string", () => {
     const result = capitalize({});
     expect(result).to.have.lengthOf(0);
     expect(result).to.equal("");
   });
 });
 
-describe('reverseString function', function() {
-    it('should reverse the string', () => {
+describe("reverseString function", function() {
+    it("should reverse the string", () => {
       const result = reverseString("hello");
       expect(result).to.have.lengthOf(5);
       expect(result).to.equal("olleh");
     });
 });
+
+describe("returns a random integer", function() {
+  it("returns a random integer in between first and second argument", () => {
+    const result = randomNumber(2, 7);
+    expect(result).to.be.a("number")
+  })
+})
