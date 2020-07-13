214ba75dd5eba122f0c5bda45f0e0e34b3e32519
Anna Morozova <anna.mik.morozova@gmail.com>
2020-07-12 22:37:41 -0400

    Feat: if/else example

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
  function capitalize(string) {
-    if (typeof string !== 'string') {
-        return ""
-    }
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
+     if (typeof string !== "string") {
+         return ""
+     }
+     return string.charAt(0).toUpperCase() + string.slice(1)
+ }
 
 
-function reverseString(str) {
-   return str.split("").reverse().join("")
-}
+ function reverseString(str) {
+     if (typeof str == "string") {
+         return str.split("").reverse().join("")
+     } else {
+         return "It is not a string";
+     }
+ }
 
-function randomNumber(min, max) {  
-    return Math.floor(Math.random() * (max - min) + min); 
-}
+ function randomNumber(min, max) {
+     return Math.floor(Math.random() * (max - min) + min);
+ }
 
-module.exports = {
-    reverseString,
-    capitalize,
-    randomNumber
-}
+
+ module.exports = {
+     reverseString,
+     capitalize,
+     randomNumber,
+ }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,31 +1,40 @@
 const expect = require("chai").expect;
-const { capitalize, reverseString, randomNumber } = require("../index");
+const {
+  capitalize,
+  reverseString,
+  randomNumber
+} = require("../index");
 
-describe("capitalize function", function() {
+describe("capitalize function", function () {
   it("should capitalize first letter of a string", () => {
     const result = capitalize("flavio");
     expect(result).to.have.lengthOf(6)
     expect(result).to.equal("Flavio");
-});
+  });
 
   it("should return empty string if the value passed is not a string", () => {
     const result = capitalize({});
-    expect(result).to.have.lengthOf(0);
     expect(result).to.equal("");
   });
 });
 
-describe("reverseString function", function() {
-    it("should reverse the string", () => {
-      const result = reverseString("hello");
-      expect(result).to.have.lengthOf(5);
-      expect(result).to.equal("olleh");
-    });
+describe("reverseString function", function () {
+  it("should reverse the string", () => {
+    const result = reverseString("hello");
+    expect(result).to.have.lengthOf(5);
+    expect(result).to.equal("olleh");
+  });
+
+  it("should return 'It is not a string' if the type of the argument is not a string", () => {
+    const result = reverseString(45);
+    expect(result).to.equal("It is not a string")
+  })
 });
 
-describe("returns a random integer", function() {
+describe("randomNumber function", function () {
   it("returns a random integer in between first and second argument", () => {
     const result = randomNumber(2, 7);
-    expect(result).to.be.a("number")
+    expect(result).to.be.a("number");
   })
 })
+
